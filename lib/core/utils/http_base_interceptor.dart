2f957dbff787bc56f97cb2e133e41a3142badf14
ishaf <shafiulislam20@gmail.com>
2024-02-19 21:44:31 +0600

    feat: updated interceptor

--- lib/core/utils/http_base_interceptor.dart
+++ lib/core/utils/http_base_interceptor.dart
@@ -1,23 +1,20 @@
-import 'package:starter_kit_flutter/core/config/env/env.dart';
-import 'package:starter_kit_flutter/core/service/remote/auth.dart';
-import 'package:starter_kit_flutter/core/utils/common_res.entity.dart';
 import 'package:dio/dio.dart';
+import 'package:starter_kit_flutter/app/app_router.dart';
 
-/// TODO: replace with local db data
-const token =
-    "beyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ0b2tlbl90eXBlIjoiYWNjZXNzIiwiZXhwIjoxNjk2ODM3Njc4LCJpYXQiOjE2OTY4MzQwNzgsImp0aSI6ImQzZjkwMjg5ZTBkMDQ4M2Q5MmJlYTFhZTQzODEyOWEyIiwidXNlcl9pZCI6ImI1NWIyYTRmLWZkMzMtNGU1YS05OTA3LTFlN2JjMmVlZTIxYyIsImVtYWlsIjoiYWRtaW5AZ21haWwuY29tIiwiaXNfc3VwZXJ1c2VyIjp0cnVlLCJpc19zdGFmZiI6dHJ1ZX0.5DWt3zIG09MOpA9aAycb-tFCzN8fuM7_l1tH7FBli1E";
-
-///
-const refresh =
-    "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ0b2tlbl90eXBlIjoicmVmcmVzaCIsImV4cCI6MTcwNDYxMDA3OCwiaWF0IjoxNjk2ODM0MDc4LCJqdGkiOiJlYTFkM2IwMDMzNjk0M2Y1YWE4OGNkYzg0NzRjMGUzZCIsInVzZXJfaWQiOiJiNTViMmE0Zi1mZDMzLTRlNWEtOTkwNy0xZTdiYzJlZWUyMWMiLCJlbWFpbCI6ImFkbWluQGdtYWlsLmNvbSIsImlzX3N1cGVydXNlciI6dHJ1ZSwiaXNfc3RhZmYiOnRydWV9.KU4GbrUqVZIIFd8kxCfe5rgJbDbpJMae5mX7AN1Jojs";
-
-/// base [Interceptor] for [Dio]. this interceptor is responsible for
-/// add `Authorization` header. and if any request got a response of `401` status code
-/// then it will try to refresh token and and then try to request
+/// Base [Interceptor] for [Dio]. This interceptor is responsible for
+/// adding the `Authorization` header. If any request receives a response with a `401` status code,
+/// it will attempt to refresh the token and then retry the request.
 class BaseInterceptor extends Interceptor {
   @override
-  void onRequest(RequestOptions options, RequestInterceptorHandler handler) {
-    options.headers['Authorization'] = 'Bearer $token';
+  Future<void> onRequest(
+      RequestOptions options,
+      RequestInterceptorHandler handler,
+      ) async {
+    final String? token  = '';
+    if (token != null) {
+      options.headers['Authorization'] = 'Bearer $token';
+    }
+
     super.onRequest(options, handler);
   }
 
@@ -33,25 +30,19 @@ class BaseInterceptor extends Interceptor {
   Future onError(DioException err, ErrorInterceptorHandler handler) async {
     if (err.response?.statusCode == 401) {
       final dio = Dio();
-      dio.options.headers['Authorization'] = 'Bearer $refresh';
-      dio.options.baseUrl = EnvProd.host;
-      final res = await RemoteAuth(dio).getRefreshToken({"refresh": refresh});
-      final data = ResponseEntity<RefreshToken>.fromJson(
-        res as Map<String, dynamic>,
-        (json) => res['data'] as RefreshToken,
-      ).data;
-      err.requestOptions.headers['Authorization'] = 'Bearer ${data.access}';
-      return handler.resolve(await dio.fetch(err.requestOptions));
+      try {
+      final String? newToken = '';
+        err.requestOptions.headers['Authorization'] =
+        'Bearer ${newToken}';
+        return handler.resolve(await dio.fetch(err.requestOptions));
+      } on DioException catch (e) {
+        if (e.response?.statusCode == 401) {
+
+          router.go('/auth');
+          return;
+        }
+      }
     }
     super.onError(err, handler);
   }
-}
-
-///
-class RefreshToken {
-  ///
-  final String access;
-
-  ///
-  RefreshToken({required this.access});
 }
